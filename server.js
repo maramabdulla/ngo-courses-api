2b0465ee37171ea62fcfe12c13eb7965b7e649bc
hamza732387 <hearthard40@gmail.com>
2020-01-14 02:27:05 -0800

    add trainee to server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const
     app = express(),
     bodyParser = require('body-parser'),
     ngo = require('./api/ngo'),
+    trainee = require('./api/trainee'),
     port = 3000,
     trainers = require('./api/trainer'),
     courses = require('./api/course')
@@ -11,11 +12,10 @@ const
 app.use(express.json({limit:'50mb'}));
 app.use(bodyParser.json());
 
-// app.use(ngo);
-// app.use(trainers);
+app.use(trainee)
 app.use(ngo);
 app.use(trainers);
-// app.use(courses);
+app.use(courses);
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
 });
