c16d6761d521b851d8493028284a5f45fd2a6cb6
ahmadammmoura <ammoura575@gmail.com>
2020-01-15 04:22:28 -0800

    fix conflict in hash.js

--- database/hash.js
+++ database/hash.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-// let bcrypt=require("bcryptjs")
-// module.exports.hashPassword =function (password,num,callback){
-//     bcrypt.hash(password,num,(err,result)=>{
-//     callback(err,result)
-//     })
-// }
-// module.exports.comparePassword=function(password,hash_password,callback){
-//     bcrypt.compare(password,hash_password,(err,result)=>{
-//     callback(err,result)
-//     })
-// module.exports.hashPassword =function (password,num,callback){
-//     bcrypt.hash(password,num,(err,result)=>{
-//     callback(err,result)
-//     })
-// }
-// module.exports.comparePassword=function(password,hash_password,callback){
-//     bcrypt.compare(password,hash_password,(err,result)=>{
-//     callback(err,result)
-//     })
-// }
-//     }
-=======
 
 let bcrypt=require("bcryptjs")
 module.exports.hashPassword =function (password,num,callback){
@@ -35,4 +12,3 @@ module.exports.comparePassword=function(password,hash_password,callback){
 
 }
 
->>>>>>> 89ce44ae5eafde1aeed9f61748e4a49069df0b48
