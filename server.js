d6687d8aea602a3bbbe0992428450af94cffb3bc
TAHANYSHRAA <tahany.shraa@gmail.com>
2020-01-22 23:55:43 -0800

    change pass DB

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(trainee)
 app.use(ngo);
 app.use(trainers);
 app.use('/imeges/trainers', express.static(__dirname + '/imeges/trainers'));
+app.use('/images/ngo', express.static(__dirname + '/images/ngo'));
+
 app.use(courses);
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`);
